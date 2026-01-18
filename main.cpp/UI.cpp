8a0208c14b1487f824e9ad63f19e18ec25fc0d9b
Penny2312 <maudkz2@gmail.com>
2026-01-18 21:49:06 +0100

    small display cleaning

--- main.cpp/UI.cpp
+++ main.cpp/UI.cpp
@@ -21,7 +21,7 @@ void UI::init() {
 
 void UI::update() {
   if (state != lastState) {
-    tft.fillScreen(COFFEE_BROWN);   // BACKGROUND CHANGED
+    tft.fillScreen(COFFEE_BROWN); 
     lastState = state;
     redraw = true;
   } else {
@@ -66,7 +66,7 @@ const char* UI::currentRatioText() {
 /* ---------- BOOT ---------- */
 void UI::drawBoot(bool redraw) {
   if (!redraw) return;
-  tft.fillScreen(COFFEE_BROWN); // BACKGROUND CHANGED
+  tft.fillScreen(COFFEE_BROWN);
   printCentered("ESPRESSO", 50, COFFEE_YELLOW, 2);
   printCentered("ASSIST", 75, COFFEE_YELLOW, 2);
   delay(1000);
@@ -89,8 +89,8 @@ void UI::drawDrinkSelect(bool redraw) {
 
   if (drink != lastDrink) {
     for (int i = 0; i < 2; i++) {
-      uint16_t bg = (i == drink) ? COFFEE_GREEN : COFFEE_BROWN;  // BACKGROUND CHANGED
-      uint16_t fg = (i == drink) ? BLACK : WHITE;               // SELECTABLE TEXT = WHITE
+      uint16_t bg = (i == drink) ? COFFEE_GREEN : COFFEE_BROWN;
+      uint16_t fg = (i == drink) ? BLACK : WHITE; 
       tft.fillRect(0, baseY + i * lineH - 2, tft.width(), lineH, bg);
       printCentered(drinks[i], baseY + i * lineH, fg, 2);
     }
@@ -121,15 +121,14 @@ void UI::drawStrengthSelect(bool redraw) {
   if (redraw) {
     printCentered("STRENGTH", 10, COFFEE_YELLOW, 2);
     lastStrength = -1;
-    printCentered(currentRatioText(), 100, COFFEE_YELLOW, 2);
   }
 
   int baseY = 45;
   if (drink == ESPRESSO) {
     if (strength != lastStrength) {
       for (int i = 0; i < 2; i++) {
-        uint16_t bg = (i == strength) ? COFFEE_GREEN : COFFEE_BROWN;  // BACKGROUND CHANGED
-        uint16_t fg = (i == strength) ? BLACK : WHITE;               // SELECTABLE TEXT = WHITE
+        uint16_t bg = (i == strength) ? COFFEE_GREEN : COFFEE_BROWN;
+        uint16_t fg = (i == strength) ? BLACK : WHITE;              
         tft.fillRect(0, baseY + i * 25 - 2, tft.width(), 22, bg);
         printCentered(labels[i], baseY + i * 25, fg, 2);
       }
@@ -161,7 +160,7 @@ void UI::drawDose(bool redraw) {
     printCentered("DOSE", 10, COFFEE_YELLOW, 2);
 
     tft.setTextSize(2);
-    tft.setTextColor(WHITE);          // SELECTABLE TEXT = WHITE
+    tft.setTextColor(WHITE);         
     tft.setCursor(10, 40); tft.print("SIN");
     tft.setCursor(79, 40); tft.print("DBL");
 
@@ -173,14 +172,14 @@ void UI::drawDose(bool redraw) {
       bool selS = (doseIndex == i);
       bool selD = (doseIndex == i + 3);
 
-      tft.fillRect(5, 65 + i * 22, 45, 20, selS ? COFFEE_GREEN : COFFEE_BROWN); // BG CHANGED
-      tft.fillRect(74, 65 + i * 22, 45, 20, selD ? COFFEE_GREEN : COFFEE_BROWN); // BG CHANGED
+      tft.fillRect(5, 65 + i * 22, 45, 20, selS ? COFFEE_GREEN : COFFEE_BROWN);
+      tft.fillRect(74, 65 + i * 22, 45, 20, selD ? COFFEE_GREEN : COFFEE_BROWN);
 
       tft.setTextSize(2);
-      tft.setTextColor(selS ? BLACK : WHITE); // SELECTABLE TEXT = WHITE
+      tft.setTextColor(selS ? BLACK : WHITE);
       tft.setCursor(10, 68 + i * 22); tft.print(single[i]); tft.print("g");
 
-      tft.setTextColor(selD ? BLACK : WHITE); // SELECTABLE TEXT = WHITE
+      tft.setTextColor(selD ? BLACK : WHITE);
       tft.setCursor(79, 68 + i * 22); tft.print(dbl[i]); tft.print("g");
     }
     lastDoseIndex = doseIndex;
@@ -204,14 +203,14 @@ void UI::drawGrind(bool redraw) {
 
   if (redraw) {
     printCentered("GRIND SIZE", 10, COFFEE_YELLOW, 2);
-    printCentered("1 - 20", 30, WHITE, 2);  // SELECTABLE TEXT = WHITE
+    printCentered("1 - 20", 30, WHITE, 2);
     lastGrind = 0;
   }
 
   if (grindSize != lastGrind) {
     char buf[8];
     snprintf(buf, sizeof(buf), "%d", grindSize);
-    tft.fillRect(0, 65, tft.width(), 30, COFFEE_BROWN); // BG CHANGED
+    tft.fillRect(0, 65, tft.width(), 30, COFFEE_BROWN);
     printCentered(buf, 70, COFFEE_GREEN, 2);
     lastGrind = grindSize;
   }
@@ -236,12 +235,12 @@ void UI::drawSummary(bool redraw) {
 
     char buf[32];
     snprintf(buf, sizeof(buf), "DOSE: %dg", dose);
-    printCentered(buf, 45, WHITE, 2);   // SELECTABLE TEXT = WHITE
+    printCentered(buf, 45, WHITE, 2); 
+
+    printCentered(currentRatioText(), 65, WHITE, 2);
 
     snprintf(buf, sizeof(buf), "OUT: %dg", (int)r.targetOutput);
-    printCentered(buf, 65, WHITE, 2);   // SELECTABLE TEXT = WHITE
-
-    printCentered(currentRatioText(), 85, WHITE, 2); // SELECTABLE TEXT = WHITE
+    printCentered(buf, 85, COFFEE_YELLOW, 2);
 
     snprintf(buf, sizeof(buf), "TIME: %ds", r.targetTime);
     printCentered(buf, 105, COFFEE_YELLOW, 2);
@@ -261,10 +260,10 @@ void UI::drawSummary(bool redraw) {
 /* ---------- BREWING ---------- */
 void UI::drawBrewing(bool redraw) {
   if (redraw) {
-    tft.fillScreen(COFFEE_BROWN); // BACKGROUND CHANGED
+    tft.fillScreen(COFFEE_BROWN);
     printCentered("BREWING...", 10, COFFEE_YELLOW, 2);
     lastShownTime = -1.0f;   // force first draw
-    printCentered("TIME", 50, WHITE, 2); // SELECTABLE TEXT = WHITE
+    printCentered("TIME", 50, WHITE, 2);
   }
   // update time using millis()
   unsigned long now = millis();
@@ -275,7 +274,7 @@ void UI::drawBrewing(bool redraw) {
   // redraw only if value changed
   if ((int)(shotTime * 10) != (int)(lastShownTime * 10)) {
     lastShownTime = shotTime;
-    tft.fillRect(0, 70, tft.width(), 30, COFFEE_BROWN); // BG CHANGED
+    tft.fillRect(0, 70, tft.width(), 30, COFFEE_BROWN);
     char buf[16];
     snprintf(buf, sizeof(buf), "%.1fs", shotTime);
     printCentered(buf, 75, COFFEE_GREEN, 2);
@@ -298,7 +297,7 @@ void UI::drawResult(bool redraw) {
     printCentered(buf, 55, COFFEE_GREEN, 2);
 
     snprintf(buf, sizeof(buf), "TARG: %ds", lastResult.targetTime);
-    printCentered(buf, 75, WHITE, 2); // SELECTABLE TEXT = WHITE
+    printCentered(buf, 75, WHITE, 2); // 
   }
 
   if (buttons.selectPressed()) {
@@ -313,13 +312,13 @@ void UI::drawAdvice(bool redraw) {
 
     if (lastResult.grindAdjustment < 0) {
       printCentered("TOO FAST", 50, RED, 2);
-      printCentered("GRIND", 80, WHITE, 2);   // SELECTABLE TEXT = WHITE
-      printCentered("FINER", 100, WHITE, 2);  // SELECTABLE TEXT = WHITE
+      printCentered("GRIND", 80, WHITE, 2);   // 
+      printCentered("FINER", 100, WHITE, 2);  // 
     }
     else if (lastResult.grindAdjustment > 0) {
       printCentered("TOO SLOW", 50, RED, 2);
-      printCentered("GRIND", 80, WHITE, 2);   // SELECTABLE TEXT = WHITE
-      printCentered("COARSER", 100, WHITE, 2); // SELECTABLE TEXT = WHITE
+      printCentered("GRIND", 80, WHITE, 2);   // 
+      printCentered("COARSER", 100, WHITE, 2); // 
     }
     else {
       printCentered("PERFECT", 65, COFFEE_GREEN, 2);
